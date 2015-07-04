106267e36a76dccf7b157e7487496989933a16bf
gam0022 <gam0022@gmail.com>
2015-07-04 14:02:30 +0900

    最小限サンプルをTypeScriptに移植

---
+++ minimum/minimum.js
@@ -0,0 +1,29 @@
+/**
+ * Created by gam0022 on 15/07/04.
+ */
+/// <reference path="../typings/tsd.d.ts"/>
+var Main = (function () {
+    function Main() {
+        var _this = this;
+        this.animate = function () {
+            requestAnimationFrame(_this.animate);
+            _this.cube.rotation.x += 0.1;
+            _this.cube.rotation.y += 0.1;
+            _this.cube.rotation.y += 0.1;
+            _this.renderer.render(_this.scene, _this.camera);
+        };
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
+        var geometry = new THREE.CubeGeometry(1, 1, 1);
+        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        this.cube = new THREE.Mesh(geometry, material);
+        this.scene.add(this.cube);
+        this.camera.position.z = 3;
+        this.animate();
+    }
+    return Main;
+})();
+//# sourceMappingURL=minimum.js.map
---
+++ minimum/minimum.ts
@@ -0,0 +1,39 @@
+/**
+ * Created by gam0022 on 15/07/04.
+ */
+/// <reference path="../typings/tsd.d.ts"/>
+
+class Main {
+    scene:THREE.Scene;
+    camera:THREE.Camera;
+    renderer:THREE.Renderer;
+    cube:THREE.Mesh;
+
+    constructor() {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
+
+        var geometry = new THREE.CubeGeometry(1, 1, 1);
+        var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        this.cube = new THREE.Mesh(geometry, material);
+        this.scene.add(this.cube);
+
+        this.camera.position.z = 3;
+
+        this.animate();
+    }
+
+    animate = () => {
+        requestAnimationFrame(this.animate);
+
+        this.cube.rotation.x += 0.1;
+        this.cube.rotation.y += 0.1;
+        this.cube.rotation.y += 0.1;
+
+        this.renderer.render(this.scene, this.camera);
+    }
+}
