b4e03036ebd6b8c576005164d9c9171b3eb5853c
gam0022 <gam0022@gmail.com>
2015-07-04 23:03:38 +0900

    GitHubの方のサンプルの方で移植しなおした

--- examples/hello.js
+++ examples/hello.js
@@ -7,9 +7,8 @@ var Hello = (function () {
         var _this = this;
         this.animate = function () {
             requestAnimationFrame(_this.animate);
-            _this.cube.rotation.x += 0.1;
-            _this.cube.rotation.y += 0.1;
-            _this.cube.rotation.y += 0.1;
+            _this.mesh.rotation.x += 0.01;
+            _this.mesh.rotation.y += 0.02;
             _this.renderer.render(_this.scene, _this.camera);
         };
         this.init();
@@ -17,15 +16,15 @@ var Hello = (function () {
     }
     Hello.prototype.init = function () {
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+        this.camera.position.z = 1000;
+        var geometry = new THREE.BoxGeometry(200, 200, 200);
+        var material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.scene.add(this.mesh);
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(this.renderer.domElement);
-        var geometry = new THREE.CubeGeometry(1, 1, 1);
-        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        this.cube = new THREE.Mesh(geometry, material);
-        this.scene.add(this.cube);
-        this.camera.position.z = 3;
     };
     return Hello;
 })();
